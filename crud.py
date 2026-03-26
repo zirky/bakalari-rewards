001a5d5ede6f6cfa9258536f469ac35f0561133c
zirky <90569126+zirky@users.noreply.github.com>
2026-03-26 04:36:14 +0100

    Add crud operations for bakalari_rewards students

--- crud.py
+++ crud.py
@@ -1,44 +1,55 @@
-# Description: This file contains the CRUD operations for talking to the database.
-
-
 from lnbits.db import Database
 from lnbits.helpers import urlsafe_short_hash
+from typing import Optional, List
 
-from .models import CreateMyExtensionData, MyExtension
+from .models import CreateBakalariStudent, BakalariStudent
 
-db = Database("ext_myextension")
+db = Database("ext_bakalari_rewards")
 
 
-async def create_myextension(data: CreateMyExtensionData) -> MyExtension:
+async def create_student(data: CreateBakalariStudent) -> BakalariStudent:
     data.id = urlsafe_short_hash()
-    await db.insert("myextension.maintable", data)
-    return MyExtension(**data.dict())
+    await db.insert("bakalari_rewards.students", data)
+    return BakalariStudent(**data.dict())
 
 
-async def get_myextension(myextension_id: str) -> MyExtension | None:
+async def get_student(student_id: str) -> Optional[BakalariStudent]:
     return await db.fetchone(
-        "SELECT * FROM myextension.maintable WHERE id = :id",
-        {"id": myextension_id},
-        MyExtension,
+        "SELECT * FROM bakalari_rewards.students WHERE id = :id",
+        {"id": student_id},
+        BakalariStudent,
     )
 
 
-async def get_myextensions(wallet_ids: str | list[str]) -> list[MyExtension]:
-    if isinstance(wallet_ids, str):
-        wallet_ids = [wallet_ids]
+async def get_students(wallet_ids: List[str]) -> List[BakalariStudent]:
     q = ",".join([f"'{w}'" for w in wallet_ids])
     return await db.fetchall(
-        f"SELECT * FROM myextension.maintable WHERE wallet IN ({q}) ORDER BY id",
-        model=MyExtension,
+        f"SELECT * FROM bakalari_rewards.students WHERE wallet IN ({q})",
+        model=BakalariStudent,
     )
 
 
-async def update_myextension(data: CreateMyExtensionData) -> MyExtension:
-    await db.update("myextension.maintable", data)
-    return MyExtension(**data.dict())
+async def get_all_students() -> List[BakalariStudent]:
+    return await db.fetchall(
+        "SELECT * FROM bakalari_rewards.students",
+        model=BakalariStudent,
+    )
 
 
-async def delete_myextension(myextension_id: str) -> None:
+async def update_student(data: CreateBakalariStudent) -> BakalariStudent:
+    await db.update("bakalari_rewards.students", data)
+    return BakalariStudent(**data.dict())
+
+
+async def delete_student(student_id: str) -> None:
     await db.execute(
-        "DELETE FROM myextension.maintable WHERE id = :id", {"id": myextension_id}
+        "DELETE FROM bakalari_rewards.students WHERE id = :id",
+        {"id": student_id},
+    )
+
+
+async def update_last_check(student_id: str, last_check: str) -> None:
+    await db.execute(
+        "UPDATE bakalari_rewards.students SET last_check = :last_check WHERE id = :id",
+        {"last_check": last_check, "id": student_id},
     )
