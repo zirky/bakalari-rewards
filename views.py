893823259392e6a27e0478c09376bdc02ef722d9
zirky <90569126+zirky@users.noreply.github.com>
2026-03-27 19:48:50 +0100

    Update views.py

--- views.py
+++ views.py
@@ -6,14 +6,12 @@ from lnbits.helpers import template_renderer
 
 bakalari_rewards_generic_router = APIRouter()
 
-
 def bakalari_rewards_renderer():
     return template_renderer(["bakalari_rewards/templates"])
-
 
 @bakalari_rewards_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
     return bakalari_rewards_renderer().TemplateResponse(
         "bakalari_rewards/index.html",
-        {"request": request, "user": user.json()},
+        {"request": request, "user": user.dict()},  # ← OPRAVENO: user.dict() místo user.json()
     )
