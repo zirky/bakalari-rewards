6967a210586def43147abdaa1bf30025ef6e29f7
zirky <90569126+zirky@users.noreply.github.com>
2026-03-27 12:22:31 +0100

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,30 @@
-// static/js/index.js
-
 window.app = Vue.createApp({
   el: '#vue',
   mixins: [window.LNbits.mixins.custom],
   data: function () {
     return {
-      // Seznam studentů pro tabulku
       students: [],
-      // Objekt pro dialog (TADY byla ta chyba 'undefined')
       formDialog: {
         show: false,
         data: {
+          name: '',
           bakalari_url: '',
           bakalari_username: '',
           bakalari_password: '',
-          reward_grade_1: 10,
-          reward_grade_2: 5,
-          reward_grade_3: 0,
-          reward_grade_4: 0,
+          reward_grade_1: 100,
+          reward_grade_2: 75,
+          reward_grade_3: 50,
+          reward_grade_4: 25,
           reward_grade_5: 0
         }
       },
-      // Nastavení tabulky
       studentsTable: {
         columns: [
-          { name: 'name', align: 'left', label: 'Student', field: 'name' },
-          { name: 'bakalari_url', align: 'left', label: 'URL školy', field: 'bakalari_url' },
-          { name: 'last_check', align: 'left', label: 'Poslední kontrola', field: 'last_check' }
+          {name: 'name', align: 'left', label: 'Jméno', field: 'name'},
+          {name: 'bakalari_url', align: 'left', label: 'URL školy', field: 'bakalari_url'},
+          {name: 'rewards', align: 'left', label: 'Odměna za známky'},
+          {name: 'last_check', align: 'left', label: 'Poslední kontrola', field: 'last_check'},
+          {name: 'id', align: 'right', label: 'Akce'}
         ],
         pagination: {
           rowsPerPage: 10
@@ -35,6 +33,7 @@ window.app = Vue.createApp({
     }
   },
   methods: {
+    // Načtení studentů z DB
     getStudents: function () {
       var self = this
       LNbits.api
@@ -50,23 +49,83 @@ window.app = Vue.createApp({
           LNbits.utils.confirmDialog(error.data.detail)
         })
     },
+
+    // Smazání studenta
+    deleteStudent: function (studentId) {
+      var self = this
+      var student = _.find(this.students, {id: studentId})
+
+      LNbits.utils
+        .confirmDialog('Opravdu chcete smazat studenta ' + student.name + '?')
+        .onOk(function () {
+          LNbits.api
+            .request(
+              'DELETE',
+              '/bakalari_rewards/api/v1/students/' + studentId,
+              self.g.user.wallets[0].adminkey
+            )
+            .then(function (response) {
+              self.students = _.reject(self.students, function (obj) {
+                return obj.id === studentId
+              })
+            })
+            .catch(function (error) {
+              LNbits.utils.confirmDialog(error.data.detail)
+            })
+        })
+    },
+
+    // Otevření dialogu pro EDITACI
+    openEditDialog: function (studentId) {
+      var student = _.find(this.students, {id: studentId})
+      // Uděláme kopii dat, aby se změny v tabulce projevily až po uložení
+      this.formDialog.data = _.clone(student)
+      this.formDialog.show = true
+    },
+
+    // Otevření dialogu pro NOVÉHO žáka
+    openFormDialog: function () {
+      this.formDialog.data = {
+        name: '',
+        bakalari_url: '',
+        bakalari_username: '',
+        bakalari_password: '',
+        reward_grade_1: 100,
+        reward_grade_2: 75,
+        reward_grade_3: 50,
+        reward_grade_4: 25,
+        reward_grade_5: 0
+      }
+      this.formDialog.show = true
+    },
+
+    // Odeslání formuláře (Nový i Editace)
     sendStudentData: function () {
       var self = this
-      // Posíláme to, co je vnořené ve formDialog.data
       var data = this.formDialog.data
+      
+      // Rozlišíme metodu podle toho, jestli už žák má ID (editace) nebo ne (nový)
+      var isUpdate = !!data.id
+      var method = isUpdate ? 'PUT' : 'POST'
+      var url = '/bakalari_rewards/api/v1/students' + (isUpdate ? '/' + data.id : '')
 
       LNbits.api
         .request(
-          'POST',
-          '/bakalari_rewards/api/v1/students',
+          method,
+          url,
           this.g.user.wallets[0].adminkey,
           data
         )
         .then(function (response) {
-          self.students.push(response.data)
+          if (isUpdate) {
+            // Najdeme v poli a nahradíme upravenými daty
+            var idx = _.findIndex(self.students, {id: data.id})
+            self.students.splice(idx, 1, response.data)
+          } else {
+            // Přidáme nového do pole
+            self.students.push(response.data)
+          }
           self.formDialog.show = false
-          // Reset dat po úspěchu
-          self.formDialog.data = { reward_grade_1: 10, reward_grade_2: 5 }
         })
         .catch(function (error) {
           LNbits.utils.confirmDialog(error.data.detail)
