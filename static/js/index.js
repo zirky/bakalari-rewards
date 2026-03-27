f6a047cf5b52bd80ab29e79b3d29d227e7693254
zirky <90569126+zirky@users.noreply.github.com>
2026-03-27 11:55:14 +0100

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,32 @@
+// static/js/index.js
+
 window.app = Vue.createApp({
   el: '#vue',
   mixins: [window.LNbits.mixins.custom],
   data: function () {
     return {
+      // Seznam studentů pro tabulku
       students: [],
-      bakalari_rewards: {
-        bakalari_url: '',
-        bakalari_username: '',
-        bakalari_password: '',
-        reward_grade_1: 0,
-        reward_grade_2: 0,
-        reward_grade_3: 0,
-        reward_grade_4: 0,
-        reward_grade_5: 0
-      },
+      // Objekt pro dialog (TADY byla ta chyba 'undefined')
       formDialog: {
         show: false,
-        data: {}
+        data: {
+          bakalari_url: '',
+          bakalari_username: '',
+          bakalari_password: '',
+          reward_grade_1: 10,
+          reward_grade_2: 5,
+          reward_grade_3: 0,
+          reward_grade_4: 0,
+          reward_grade_5: 0
+        }
       },
+      // Nastavení tabulky
       studentsTable: {
         columns: [
-          {name: 'name', align: 'left', label: 'Student', field: 'name'},
-          {name: 'bakalari_url', align: 'left', label: 'URL školy', field: 'bakalari_url'},
-          {name: 'last_check', align: 'left', label: 'Poslední kontrola', field: 'last_check'}
+          { name: 'name', align: 'left', label: 'Student', field: 'name' },
+          { name: 'bakalari_url', align: 'left', label: 'URL školy', field: 'bakalari_url' },
+          { name: 'last_check', align: 'left', label: 'Poslední kontrola', field: 'last_check' }
         ],
         pagination: {
           rowsPerPage: 10
@@ -31,13 +35,12 @@ window.app = Vue.createApp({
     }
   },
   methods: {
-    // Získání seznamu studentů
     getStudents: function () {
       var self = this
       LNbits.api
         .request(
           'GET',
-          '/bakalari_rewards/api/v1/students', // Opravená cesta
+          '/bakalari_rewards/api/v1/students',
           this.g.user.wallets[0].adminkey
         )
         .then(function (response) {
@@ -47,61 +50,27 @@ window.app = Vue.createApp({
           LNbits.utils.confirmDialog(error.data.detail)
         })
     },
-    // Smazání studenta
-    deleteStudent: function (studentId) {
-      var self = this
-      var student = _.find(this.students, {id: studentId})
-
-      LNbits.utils
-        .confirmDialog('Opravdu chcete smazat studenta ' + student.name + '?')
-        .onOk(function () {
-          LNbits.api
-            .request(
-              'DELETE',
-              '/bakalari_rewards/api/v1/students/' + studentId, // Opravená cesta
-              self.g.user.wallets[0].adminkey
-            )
-            .then(function (response) {
-              self.students = _.reject(self.students, function (obj) {
-                return obj.id === studentId
-              })
-            })
-            .catch(function (error) {
-              LNbits.utils.confirmDialog(error.data.detail)
-            })
-        })
-    },
-    // Vytvoření nového studenta (odeslání formuláře)
     sendStudentData: function () {
       var self = this
+      // Posíláme to, co je vnořené ve formDialog.data
       var data = this.formDialog.data
 
       LNbits.api
         .request(
           'POST',
-          '/bakalari_rewards/api/v1/students', // Opravená cesta
+          '/bakalari_rewards/api/v1/students',
           this.g.user.wallets[0].adminkey,
           data
         )
         .then(function (response) {
           self.students.push(response.data)
           self.formDialog.show = false
-          self.formDialog.data = {} // Reset formuláře
+          // Reset dat po úspěchu
+          self.formDialog.data = { reward_grade_1: 10, reward_grade_2: 5 }
         })
         .catch(function (error) {
           LNbits.utils.confirmDialog(error.data.detail)
         })
-    },
-    // Otevření dialogu pro přidání studenta
-    openFormDialog: function () {
-      this.formDialog.data = {
-        reward_grade_1: 10, // Výchozí hodnoty v satoshi
-        reward_grade_2: 5,
-        reward_grade_3: 0,
-        reward_grade_4: 0,
-        reward_grade_5: 0
-      }
-      this.formDialog.show = true
     }
   },
   created: function () {
