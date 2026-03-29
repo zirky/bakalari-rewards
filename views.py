ad8fd351e0e8510535ab873845d041429fb08b20
zirky <90569126+zirky@users.noreply.github.com>
2026-03-29 15:21:06 +0200

    Refactor index function for better readability

--- views.py
+++ views.py
@@ -11,8 +11,7 @@ def bakalari_rewards_renderer():
 
 @bakalari_rewards_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
-   return bakalari_rewards_renderer().TemplateResponse(
-    "bakalari_rewards/index.html",
-    {"request": request, "user": user.json() if hasattr(user, 'json') else user.dict()},
-)
+    return bakalari_rewards_renderer().TemplateResponse(
+        "bakalari_rewards/index.html",
+        {"request": request, "user": user.json() if hasattr(user, 'json') else user.dict()},
     )
