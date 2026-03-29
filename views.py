d207f16a3a2f2ff10718218d61adc125b40bff8f
zirky <90569126+zirky@users.noreply.github.com>
2026-03-29 15:19:10 +0200

    Update user data handling in index response

--- views.py
+++ views.py
@@ -11,7 +11,8 @@ def bakalari_rewards_renderer():
 
 @bakalari_rewards_generic_router.get("/", response_class=HTMLResponse)
 async def index(request: Request, user: User = Depends(check_user_exists)):
-    return bakalari_rewards_renderer().TemplateResponse(
-        "bakalari_rewards/index.html",
-        {"request": request, "user": user},
+   return bakalari_rewards_renderer().TemplateResponse(
+    "bakalari_rewards/index.html",
+    {"request": request, "user": user.json() if hasattr(user, 'json') else user.dict()},
+)
     )
